7ea504ba37cd34cebfc47f381fd425f72f17011b
raghaves <raghaves@microsoft.com>
2020-06-16 10:23:23 +0530

    Review fix

--- __tests__/run.test.ts
+++ __tests__/run.test.ts
@@ -1,4 +1,4 @@
-import {run} from '../src/login'
+import { run } from '../src/login'
 
 describe('This is a placeholder for intial test cases, to be removed', () => {
     test('Dummy test case', async () => {
