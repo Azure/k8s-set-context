959bbfb69a155c17d3beffb5a245feaf8de70c81
Tatsinnit <tamishra@microsoft.com>
2025-03-20 04:12:40 +1300

    Fix for client-node ESM v Commonjs issue. (#132)
    Signed-off-by: Tatsat Mishra <tamishra@microsoft.com>

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+// babel.config.js
+export default {
+   presets: [
+      '@babel/preset-env', // For handling ES modules
+      '@babel/preset-typescript' // For handling TypeScript
+   ]
+}
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
    restoreMocks: true,
    clearMocks: true,
    resetMocks: true,
@@ -6,8 +6,16 @@ module.exports = {
    testEnvironment: 'node',
    testMatch: ['**/*.test.ts'],
    transform: {
-      '^.+\\.ts$': 'ts-jest'
+      '^.+\\.ts$': 'ts-jest', // Use ts-jest for TypeScript files
+      '^.+\\.js$': 'babel-jest' // Transform TypeScript files
    },
+   transformIgnorePatterns: [
+      '/node_modules/(?!@kubernetes/client-node)/' // Make sure to transform the Kubernetes client module
+   ],
+   moduleNameMapper: {
+      '^.+\\.css$': 'jest-transform-stub' // Handle CSS imports (if any)
+   },
+   extensionsToTreatAsEsm: ['.ts', '.tsx'], // Treat TypeScript files as ESM
    verbose: true,
    coverageThreshold: {
       global: {
