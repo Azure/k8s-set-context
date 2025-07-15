0a14af326610daea9b46b904380a22fb3a8e139f
Betsy George <betsygeo34@gmail.com>
2025-07-15 15:40:35 -0400

    prettier edits

--- src/kubeconfigs/default.test.ts
+++ src/kubeconfigs/default.test.ts
@@ -1,14 +1,14 @@
-import * as fs from 'fs';
-import { getRequiredInputError } from '../../tests/util';
-import { createKubeconfig, getDefaultKubeconfig } from './default';
+import * as fs from 'fs'
+import {getRequiredInputError} from '../../tests/util'
+import {createKubeconfig, getDefaultKubeconfig} from './default'
 
 describe('Default kubeconfig', () => {
    test('it creates a kubeconfig with proper format', () => {
-      const certAuth = 'certAuth';
-      const token = 'token';
-      const clusterUrl = 'clusterUrl';
+      const certAuth = 'certAuth'
+      const token = 'token'
+      const clusterUrl = 'clusterUrl'
 
-      const kc = createKubeconfig(certAuth, token, clusterUrl);
+      const kc = createKubeconfig(certAuth, token, clusterUrl)
       const expected = JSON.stringify({
          apiVersion: 'v1',
          kind: 'Config',
@@ -18,106 +18,106 @@ describe('Default kubeconfig', () => {
                cluster: {
                   server: clusterUrl,
                   'certificate-authority-data': certAuth,
-                  'insecure-skip-tls-verify': false,
-               },
-            },
+                  'insecure-skip-tls-verify': false
+               }
+            }
          ],
-         users: [{ name: 'default-user', user: { token } }],
+         users: [{name: 'default-user', user: {token}}],
          contexts: [
             {
                name: 'loaded-context',
                context: {
                   cluster: 'default',
                   user: 'default-user',
-                  name: 'loaded-context',
-               },
-            },
+                  name: 'loaded-context'
+               }
+            }
          ],
          preferences: {},
-         'current-context': 'loaded-context',
-      });
-      expect(kc).toBe(expected);
-   });
+         'current-context': 'loaded-context'
+      })
+      expect(kc).toBe(expected)
+   })
 
    test('it throws error without method', () => {
       expect(() => getDefaultKubeconfig()).toThrow(
          getRequiredInputError('method')
-      );
-   });
+      )
+   })
 
    describe('default method', () => {
       beforeEach(() => {
-         process.env['INPUT_METHOD'] = 'default';
-      });
+         process.env['INPUT_METHOD'] = 'default'
+      })
 
       test('it throws error without kubeconfig', () => {
          expect(() => getDefaultKubeconfig()).toThrow(
             getRequiredInputError('kubeconfig')
-         );
-      });
+         )
+      })
 
       test('it gets default config through kubeconfig input', () => {
-         const kc = 'example kc';
-         process.env['INPUT_KUBECONFIG'] = kc;
+         const kc = 'example kc'
+         process.env['INPUT_KUBECONFIG'] = kc
 
-         expect(getDefaultKubeconfig()).toBe(kc);
-      });
+         expect(getDefaultKubeconfig()).toBe(kc)
+      })
 
       test('it gets default config through base64 kubeconfig input', () => {
-         const kc = 'example kc';
-         const base64Kc = Buffer.from(kc, 'utf-8').toString('base64');
-         process.env['INPUT_KUBECONFIG'] = base64Kc;
-         process.env['INPUT_KUBECONFIG_ENCODING'] = 'base64';
+         const kc = 'example kc'
+         const base64Kc = Buffer.from(kc, 'utf-8').toString('base64')
+         process.env['INPUT_KUBECONFIG'] = base64Kc
+         process.env['INPUT_KUBECONFIG_ENCODING'] = 'base64'
 
-         expect(getDefaultKubeconfig()).toBe(kc);
-      });
-   });
+         expect(getDefaultKubeconfig()).toBe(kc)
+      })
+   })
 
    test('it defaults to default method', () => {
-      process.env['INPUT_METHOD'] = 'unknown';
+      process.env['INPUT_METHOD'] = 'unknown'
 
-      const kc = 'example kc';
-      process.env['INPUT_KUBECONFIG'] = kc;
+      const kc = 'example kc'
+      process.env['INPUT_KUBECONFIG'] = kc
 
-      expect(getDefaultKubeconfig()).toBe(kc);
-   });
+      expect(getDefaultKubeconfig()).toBe(kc)
+   })
 
    test('it defaults to default method from service-principal', () => {
-      process.env['INPUT_METHOD'] = 'service-principal';
+      process.env['INPUT_METHOD'] = 'service-principal'
 
-      const kc = 'example kc';
-      process.env['INPUT_KUBECONFIG'] = kc;
+      const kc = 'example kc'
+      process.env['INPUT_KUBECONFIG'] = kc
 
-      expect(getDefaultKubeconfig()).toBe(kc);
-   });
+      expect(getDefaultKubeconfig()).toBe(kc)
+   })
 
    describe('service-account method', () => {
       beforeEach(() => {
-         process.env['INPUT_METHOD'] = 'service-account';
-      });
+         process.env['INPUT_METHOD'] = 'service-account'
+      })
 
       test('it throws error without cluster url', () => {
          expect(() => getDefaultKubeconfig()).toThrow(
             getRequiredInputError('k8s-url')
-         );
-      });
+         )
+      })
 
       test('it throws error without k8s secret', () => {
-         process.env['INPUT_K8S-URL'] = 'url';
+         process.env['INPUT_K8S-URL'] = 'url'
 
          expect(() => getDefaultKubeconfig()).toThrow(
             getRequiredInputError('k8s-secret')
-         );
-      });
+         )
+      })
 
       test('it gets kubeconfig through service-account', () => {
-         const k8sUrl = 'https://testing-dns-4za.hfp.earth.azmk8s.io:443';
-         const token = 'ZXlKaGJHY2lPcUpTVXpJMU5pSX=';
-         const cert = 'LS0tLS1CRUdJTiBDRWyUSUZJQ';
-         const k8sSecret = fs.readFileSync('tests/sample-secret.yml').toString();
+         const k8sUrl = 'https://testing-dns-4za.hfp.earth.azmk8s.io:443'
+         const token = 'ZXlKaGJHY2lPcUpTVXpJMU5pSX='
+         const cert = 'LS0tLS1CRUdJTiBDRWyUSUZJQ'
+         const k8sSecret = fs.readFileSync('tests/sample-secret.yml').toString()
 
-         process.env['INPUT_K8S-URL'] = k8sUrl;
-         process.env['INPUT_K8S-SECRET'] = k8sSecret;
+         process.env['INPUT_K8S-URL'] = k8sUrl
+         process.env['INPUT_K8S-SECRET'] = k8sSecret
 
          const expectedConfig = JSON.stringify({
             apiVersion: 'v1',
@@ -128,15 +128,15 @@ describe('Default kubeconfig', () => {
                   cluster: {
                      server: k8sUrl,
                      'certificate-authority-data': cert,
-                     'insecure-skip-tls-verify': false,
-                  },
-               },
+                     'insecure-skip-tls-verify': false
+                  }
+               }
             ],
             users: [
                {
                   name: 'default-user',
-                  user: { token: Buffer.from(token, 'base64').toString() },
-               },
+                  user: {token: Buffer.from(token, 'base64').toString()}
+               }
             ],
             contexts: [
                {
@@ -144,15 +144,15 @@ describe('Default kubeconfig', () => {
                   context: {
                      cluster: 'default',
                      user: 'default-user',
-                     name: 'loaded-context',
-                  },
-               },
+                     name: 'loaded-context'
+                  }
+               }
             ],
             preferences: {},
-            'current-context': 'loaded-context',
-         });
+            'current-context': 'loaded-context'
+         })
 
-         expect(getDefaultKubeconfig()).toBe(expectedConfig);
-      });
-   });
-});
+         expect(getDefaultKubeconfig()).toBe(expectedConfig)
+      })
+   })
+})
